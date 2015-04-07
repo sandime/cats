e3d945a9eca01064ffd65df39c5fe9ac3515a0ff
sherri <sherri@sherricruz.com>
2015-04-06 17:16:07 -0700

    experimenting with server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Created by SHERRI on 3/21/15.
  */
     //simple server
+    /*
 var express = require('express');
 var app = express();
 var path = require ('path');
@@ -10,3 +11,41 @@ app.get('/', function (req, res) {
 });
 app.listen(1337);
 console.log('1337 is the magic port');
+*/
+
+//mean book sod server
+var express = require('express');
+var app = express();
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    if (req.url === '/favicon.ico') {
+        return res.end();
+    }
+    console.log('Incoming request to ' + req.url);
+
+    var i = 2;
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    setTimeout(function() {
+        fs.readFile(__filename, {
+            encoding: 'utf8'
+        }, function (error, contents) {
+
+            if (error) {
+                console.error(error);
+                return res.end();
+            }
+
+            console.log('sending response for ' + req.url);
+            res.end(contents);
+        });
+    }, 5000);
+
+    while(i--) {
+        console.log('Loop value: '  + i + '\r');
+    }
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
